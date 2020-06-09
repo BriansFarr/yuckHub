6dfeb1108c66a64b04243700c4ceeed8afd83acc
Brian Farr <64746326+BriansFarr@users.noreply.github.com>
2020-06-09 19:55:12 -0400

    working through api issues

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 async function getData() {
-  const response = await fetch(`https://data.cityofnewyork.us/resource/43nn-pn8j.json?`);
+  const response = await fetch(`https://data.cityofnewyork.us/resource/43nn-pn8j.json?dba=IVY`);
   const data = await response.json();
+  
   console.log(data);
 }
 
 getData();
 
+let mainContainer = document.getElementById("data");
 
-// https://data.cityofnewyork.us/resource/43nn-pn8j.json
 
-///*const nameUrl = "https://data.cityofnewyork.us/resource/43nn-pn8j.json?dba=";
 
-//const restName = document.getElementById("blank").value;
+/*https://data.cityofnewyork.us/resource/43nn-pn8j.json
 
-//function getRest() {
- // const response = await fetch(`${nameUrl}+${restName}`);
- // const data = await response.json();
- // console.log(data);
-//}
+const nameUrl = "https://data.cityofnewyork.us/resource/43nn-pn8j.json?dba=";
 
-//getRest();
+const restName = document.getElementById("blank").value;
+
+button.addEventListener('click', getRest() {
+  const response = await fetch(`${nameUrl}` + `${restName}`);
+  const data = await response.json();
+  console.log(data);
+});
+getRest();
+*/
+
+
+
+
+
