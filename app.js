259f92e38e3a082e81fe98b0c61953cfaaf446ea
Brian Farr <64746326+BriansFarr@users.noreply.github.com>
2020-06-10 16:24:40 -0400

    css styling started

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 async function getData() {
-  //console.log("are we here")
+  //console.log("am i here")
+  //sanitize search input
   let place = document.getElementById("searchInput").value.toUpperCase();
   //console.log(place)
+  // fetch data and append input to endpoint
+  
   const response = await fetch(`https://data.cityofnewyork.us/resource/43nn-pn8j.json?dba=` + place);
   const data = await response.json();
   
+  //push restaurant name from object and set under Name p
   const restName = document.getElementById("Name");
   restName.innerHTML = data[0].dba;
 
@@ -13,45 +17,30 @@ async function getData() {
 
   const violations = document.getElementById("violations");
   violations.innerHTML = data[0].violation_description;
-  violations1.innerHTML = data[1].violation_description;
-  violations2.innerHTML = data[2].violation_description;
   
   const address = document.getElementById("address");
-  address.innerHTML = [data[0].building +" "+ data[0].street + " "+ data[0].boro];
+  address.innerHTML = [data[0].building +" "+ data[0].street + " "+ data[0].boro + " "+ data[0].zipcode];
   
   const cuisine = document.getElementById("cuisine");
-  cuisine.innerHTML = data[0].CUISINE_DESCRIPTION
+  cuisine.innerHTML = data[0].cuisine_description;
+
+  const restScore = document.getElementById("score");
+  restScore.innerHTML = data[0].score;
     
   }
  
 
-  // now push first object in the array of data into an empty object then return that variable
-
  
-
-
+///set event listener to button to listen to onclick and trigger getData function
 document.getElementById("searchBtn").addEventListener('click', event => {
   getData();
 });
 
 
-// getData()
 
 
 
-/*https://data.cityofnewyork.us/resource/43nn-pn8j.json
 
-const nameUrl = "https://data.cityofnewyork.us/resource/43nn-pn8j.json?dba=";
-
-const restName = document.getElementById("blank").value;
-
-button.addEventListener('click', getRest() {
-  const response = await fetch(`${nameUrl}` + `${restName}`);
-  const data = await response.json();
-  console.log(data);
-});
-getRest();
-*/
 
 
 
